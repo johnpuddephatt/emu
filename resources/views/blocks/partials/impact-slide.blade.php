b67be0067cb8da85f0d498ac6ca9e8cc6eb02391
John Puddephatt <john@jdp.org.uk>
2026-08-18 22:24:50 +0100

    image rounding

--- resources/views/blocks/partials/impact-slide.blade.php
+++ resources/views/blocks/partials/impact-slide.blade.php
@@ -1,10 +1,8 @@
 @if ($slide['image'])
-    {!!
-        wp_get_attachment_image($slide['image'], 'large', false, [
-            'class' => 'aspect-[4/5] w-full object-cover',
-            'sizes' => '(min-width: 1280px) 20vw, (min-width: 640px) 40vw, 80vw',
-        ])
-    !!}
+    {!! wp_get_attachment_image($slide['image'], 'large', false, [
+        'class' => 'aspect-[4/5] w-full rounded-xl object-cover',
+        'sizes' => '(min-width: 1280px) 20vw, (min-width: 640px) 40vw, 80vw',
+    ]) !!}
 @else
     <div class="bg-gray-light aspect-[4/5] w-full"></div>
 @endif
